4b8d1602137dcf4791fdef978392e7411c35ea79
fangfangxu <472848022@qq.com>
2020-07-14 23:39:56 +0800

    commit

--- 01-primary/src/main/java/com/abc/server/SomeChannelInitializer.java
+++ 01-primary/src/main/java/com/abc/server/SomeChannelInitializer.java
@@ -15,7 +15,7 @@ public class SomeChannelInitializer extends ChannelInitializer<SocketChannel> {
        //从Channel中获取pipeline
         ChannelPipeline pipeline = ch.pipeline();
         //将HttpServerCodec处理器放入到pipeline的最后
-        //HttpServerCodec是什么？HttpRequestDecoder与HttpResponseEncoder的复合体
+        //HttpServerCodec是什么？HttpRequestDecoder  与HttpResponseEncoder的复合体
         //HttpRequestDecoder Http请求解码器，将Channel中的ByteBuf数据解码为HttpRequest对象
         //HttpResponseEncoder Http请求编码器，将HttpResponse对象编码为将要在Channel中发送的ByteBuf数据
         //Client        管道           Server
